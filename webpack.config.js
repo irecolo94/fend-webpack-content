2ef176114e48d10990db8149d2c426dcfbc8e134
irecolo94 <ireblue@virgilio.it>
2021-02-07 14:03:38 +0100

    install babel make import export

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,13 @@ const webpack = require('webpack')
 
 module.exports = {
   entry: './src/client/index.js',
+  module: {
+        rules: [
+                {
+            test: '/\.js$/',
+            exclude: /node_modules/,  //excludes files/directories to be used in a loader
+            loader: "babel-loader"
+                }
+              ]
+         }
 }
